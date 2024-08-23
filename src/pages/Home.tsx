eb2d1c78a8d74f9f39ec172262c39a24ac10b028
Franklin <131524766+frank245chikanku@users.noreply.github.com>
2024-08-23 14:43:46 +0200

    uni-life

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,7 @@ import Hero from "../components/home/Hero";
 
 function Home() {
   return (
-    <div className="relative bg-custom flex flex-col bg-[#0b0b26] items-center justify-center text-white overflow-hidden">
-      <div className="absolute inset-0">
-        <div className="absolute inset-0 opacity-50 mix-blend-overlay pointer-events-none noise-texture"></div>
-      </div>
+    <div className="w-full flex flex-col bg-[#0b0b26]">
       <Navbar />
       <Hero />
       <Categories />
