07af9ce1f5e427f00c107f01f8bf987fe5c3003e
Franklin <131524766+frank245chikanku@users.noreply.github.com>
2024-09-25 11:24:14 +0200

    changes

--- src/components/academic/Chapter1.tsx
+++ src/components/academic/Chapter1.tsx
@@ -6,11 +6,14 @@ const Phase1: React.FC = () => {
       <h2>Phase 1: Understanding Academic Integrity</h2>
       <p>
         Academic integrity is an interdisciplinary concept that provides the foundation for every aspect and all levels of education. 
-        The term evokes strong emotions in teachers, researchers, and students—not least because it is usually associated with negative behaviors. When considering academic integrity, 
-        the discussion tends to revolve around cheating, plagiarism, dishonesty, fraud, and other academic malpractice and how best to prevent these behaviors. 
-        A more productive approach entails a focus on promoting the positive values of honesty, trust, fairness, respect, responsibility,
-         and courage as the intrinsically motivated drivers for ethical academic practice. 
-         Academic integrity is much more than “a student issue” and requires commitment from all stakeholders in the academic community, 
+        The term evokes strong emotions in teachers, researchers, and students—not least because it is usually associated with negative behaviors.
+      </p>
+      <p>
+        When considering academic integrity, the discussion tends to revolve around cheating, plagiarism, dishonesty, fraud, and other academic malpractice and how best to prevent these behaviors.
+        A more productive approach entails a focus on promoting the positive values of honesty, trust, fairness, respect, responsibility, and courage as the intrinsically motivated drivers for ethical academic practice.
+      </p>
+      <p>
+        Academic integrity is much more than “a student issue” and requires commitment from all stakeholders in the academic community, 
         including undergraduate and postgraduate students, teachers, established researchers, 
         senior managers, policymakers, support staff, and administrators.
       </p>
