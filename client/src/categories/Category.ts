c0a31a53358593e630f611873b6b3ca17ac9239c
franklin <franklin@frank>
2024-11-26 16:33:13 +0200

    changes

--- client/src/categories/Category.ts
+++ client/src/categories/Category.ts
@@ -1,13 +1,13 @@
-import { academicsData } from "../data/academicsData";
-import { peerData } from "../data/peerData";
+// import { academicsData } from "../data/academicsData";
+// import { peerData } from "../data/peerData";
 
-// Mapping category names to their respective data files
-const categoryMap: { [key: string]: any } = {
-  academics: academicsData,
-  peer: peerData,
-  // Add other categories here
-};
+// // Mapping category names to their respective data files
+// const categoryMap: { [key: string]: any } = {
+//   academics: academicsData,
+//   peer: peerData,
+//   // Add other categories here
+// };
 
-export const getCategoryData = (category: string) => {
-  return categoryMap[category];
-};
+// export const getCategoryData = (category: string) => {
+//   return categoryMap[category];
+// };
